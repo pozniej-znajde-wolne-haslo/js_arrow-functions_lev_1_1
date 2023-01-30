1ba727569a02b289ee206c012cfc076305ee34fd
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-30 13:53:25 +0100

    adds html- and js-files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function intro_fn() {
+  console.log("Hello World");
+} 
+
+intro_fn();
+
+// als arrow function
+
+const intro_arrow = () => {
+  console.log("Hello World");
+}
+
+intro_arrow();
+
+// verkürzte version
+
+intro_arrow_short = () => console.log("Hello World");
+
+intro_arrow_short();
